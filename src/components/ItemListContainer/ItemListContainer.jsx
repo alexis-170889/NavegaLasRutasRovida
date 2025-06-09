924e0c8e419f0b21c4f7de719feb3deb4d6eaeed
Alexis Rovida <alexisrovida@gmail.com>
2025-06-09 20:35:19 -0300

    NavegaLasRutasRovida

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,17 @@
+import Item from '../Item/Item';
 import './ItemListContainer.css';
 
 function ItemListContainer({greetings}) {
     return (
-        <h1>{greetings}</h1>
+        <>
+        <h2>{greetings}</h2>
+        <Item
+        title= 'Conjunto Negro'
+        text= 'Conjunto Encaje Negro'
+        price= '$15000'
+        img= 'https://img.freepik.com/foto-gratis/arreglo-lenceria-femenina-bodegon_23-2150411274.jpg?semt=ais_hybrid&w=740'
+        />
+        </>
     );
 };
 
